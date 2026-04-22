469eb48a1548befd2985111347a045a97438883d
Habib Mukhlis Triatmojo <89024106+Htech5@users.noreply.github.com>
2026-04-23 03:22:22 +0700

    Update loanRoutes.js

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -4,8 +4,8 @@ import { LoanController } from '../controllers/loanController.js';
 const router = express.Router();
 
 router.get('/', LoanController.getLoans);
+router.get('/top-borrowers', LoanController.getTopBorrowers);
 router.post('/', LoanController.createLoan);
 router.put('/:id/return', LoanController.returnBook);
-router.get('/top-borrowers', LoanController.getTopBorrowers);
 
 export default router;
